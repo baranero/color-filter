73babb6c34e549c9e5b7223d66818cfe735b4ef7
baranero <jakub.baran2@gmail.com>
2023-04-03 23:42:33 +0200

    added NewColor form

--- src/components/NewColor.tsx
+++ src/components/NewColor.tsx
@@ -1,10 +1,33 @@
+import React, { useState } from 'react'
 import classes from './NewColor.module.scss'
 
 const NewColor = () => {
 
+  const [enteredColor, setEnteredColor] = useState<string>('')
+
+  const colorChangeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setEnteredColor(event.target.value)
+  }
+
+  console.log(enteredColor)
+
+  const submitHandler = (event: React.FormEvent): void => {
+    event.preventDefault()
+    localStorage.setItem("enteredColor", enteredColor)
+    console.log(localStorage.getItem("enteredColor"));
+    
+  }
+  
+
   return (
-    <form>
-      <input type='text'/>
+    <form onSubmit={submitHandler}>
+      <label htmlFor='color'>Color:</label>
+      <input
+        id='color'
+        type='text'
+        value={enteredColor}
+        onChange={colorChangeHandler}
+      />
       <button type='submit'>Submit</button>
     </form>
   )
