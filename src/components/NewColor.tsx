7053da4c871df858840bd0970cf094fed609a98e
baranero <jakub.baran2@gmail.com>
2023-04-03 23:11:58 +0200

    first commit

---
+++ src/components/NewColor.tsx
@@ -0,0 +1,13 @@
+import classes from './NewColor.module.scss'
+
+const NewColor = () => {
+
+  return (
+    <form>
+      <input type='text'/>
+      <button type='submit'>Submit</button>
+    </form>
+  )
+}
+
+export default NewColor
