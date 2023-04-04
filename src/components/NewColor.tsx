ffbf3d94c7026d5e49c89838d95d7c6d4d1d7c2a
baranero <jakub.baran2@gmail.com>
2023-04-04 19:35:10 +0200

    threw error while value is wrong

--- src/components/NewColor.tsx
+++ src/components/NewColor.tsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './NewColor.module.scss'
 
 const NewColor = () => {
 
   const [enteredColor, setEnteredColor] = useState<string>('')
 
-  const colorChangeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setEnteredColor(event.target.value)
-  }
+  let inputValueisIncorrect: boolean = enteredColor.includes('#', 1)
 
-  console.log(enteredColor)
+    const colorChangeHandler = (event: React.ChangeEvent<HTMLInputElement>): void => {
+      if (inputValueisIncorrect) {
+     
+        setEnteredColor(enteredColor.slice(0, -1))
+      } else {
+        setEnteredColor(event.target.value)
+      }
+      
+    }  
 
   const submitHandler = (event: React.FormEvent): void => {
     event.preventDefault()
     localStorage.setItem("enteredColor", enteredColor)
     console.log(localStorage.getItem("enteredColor"));
-    
   }
   
 
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor='color'>Color:</label>
+    <form onSubmit={submitHandler} className={classes['color-form']} >
+      <label htmlFor='color' className={classes['color-form-label']} >Color:</label>
       <input
+        className={classes[inputValueisIncorrect ? 'color-form-input-wrong' : 'color-form-input']}
         id='color'
         type='text'
         value={enteredColor}
         onChange={colorChangeHandler}
+        placeholder='Input color in HEX format (eg. #3F3F3F)'
+        maxLength={7}
       />
-      <button type='submit'>Submit</button>
+      {!inputValueisIncorrect ? '' : <p className={classes['color-form-warning']} >You can use # sign only as first</p>}
+      <button
+      className={classes['color-form-button']} type='submit'>Submit</button>
     </form>
   )
 }
