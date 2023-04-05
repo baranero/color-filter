e57a2fe4eadea3d4513d64302c61322bd784bd8c
baranero <jakub.baran2@gmail.com>
2023-04-05 12:36:44 +0200

    display from localStorage and color with scss

--- src/components/ColorsContainer.tsx
+++ src/components/ColorsContainer.tsx
@@ -9,27 +9,29 @@ const ColorsContainer = () => {
   document.documentElement.style.setProperty("--color-green", defaultColors[1].color);
   document.documentElement.style.setProperty("--color-blue", defaultColors[2].color);
 
-  const [colorsFromLocalStorage, setColorsFromLocalStorage] = useState<Colors[]>([])
+ 
 
+  const colorsArrayLocalStorage = (JSON.parse(localStorage.getItem('enteredColors') || "[]"))
+  
   useEffect(() => {
-    const colorsFromLocalStorage = JSON.parse(window.localStorage.getItem('enteredColor') || "[]")
-    if (colorsFromLocalStorage) {
-      setColorsFromLocalStorage(colorsFromLocalStorage)
+    document.documentElement.style.setProperty(`--number-of-child`, colorsArrayLocalStorage.length + 3)
+    if (colorsArrayLocalStorage !== null) { 
+      for (let i=0; i<colorsArrayLocalStorage.length; i++) {
+        document.documentElement.style.setProperty(`--color-user-${i+4}`, colorsArrayLocalStorage[i].color)
+      }
     }
-  }, [])
+  })
 
-  const colorsArrayLocalStorage = (JSON.parse(localStorage.getItem('enteredColor') || "[]"))
-  console.log(colorsFromLocalStorage);
-  
-  
+
+  console.log(colorsArrayLocalStorage);  
 
 return (
   <div className={classes['colors-container']}>
     <div className={classes['colors-container-default-red']}></div>
     <div className={classes['colors-container-default-green']}></div>
     <div className={classes['colors-container-default-blue']}></div>
-    {colorsFromLocalStorage.map((item: Colors) => {
-      return <div key={item.color} className={classes['colors-container-user']}>{item.color}</div>
+    {colorsArrayLocalStorage.map((item: Colors) => {
+      return <><div key={item.id} className={classes['colors-container']}><p>{item.color}</p></div></>
     })}
   </div>
 )
