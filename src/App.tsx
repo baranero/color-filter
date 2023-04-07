736f70ae30ac4b5f0d7b535ac9890020da1686f3
baranero <jakub.baran2@gmail.com>
2023-04-07 16:52:18 +0200

    added gh page

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ const App: React.FC = () => {
     }
 
 
-  }, [colorsArray.length, colorsFromLocalStorage?.length])
+  }, [colorsArray?.length, colorsFromLocalStorage?.length])
 
   useEffect(() => {
 
